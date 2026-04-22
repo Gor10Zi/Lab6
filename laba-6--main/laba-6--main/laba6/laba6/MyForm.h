30f63df39486d54b1d5077365a88d10136c10937
Gor10Zi <bnatasa447@gmail.com>
2026-04-22 14:58:01 +0300

    Add files via upload

---
+++ laba-6--main/laba-6--main/laba6/laba6/MyForm.h
@@ -0,0 +1,322 @@
+﻿#pragma once
+
+namespace laba6 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^ проПрограмуToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ умоваToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ проАвтораToolStripMenuItem;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->проПрограмуToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->умоваToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->проАвтораToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->menuStrip1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::AliceBlue;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button1->Location = System::Drawing::Point(171, 379);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(140, 34);
+			this->button1->TabIndex = 26;
+			this->button1->Text = L"Ввести";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(561, 98);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(86, 20);
+			this->label6->TabIndex = 25;
+			this->label6->Text = L"Всі книги";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(853, 98);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(194, 20);
+			this->label5->TabIndex = 24;
+			this->label5->Text = L"Відфільтровані книги";
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(804, 130);
+			this->textBox6->Multiline = true;
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(270, 242);
+			this->textBox6->TabIndex = 23;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(464, 130);
+			this->textBox5->Multiline = true;
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(270, 242);
+			this->textBox5->TabIndex = 22;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(198, 270);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->TabIndex = 21;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(198, 235);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 20;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(198, 191);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 19;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(198, 142);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(237, 20);
+			this->textBox1->TabIndex = 18;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(81, 269);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(110, 18);
+			this->label4->TabIndex = 17;
+			this->label4->Text = L"Введений рік";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(81, 235);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(101, 18);
+			this->label3->TabIndex = 16;
+			this->label3->Text = L"Рік видання";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(81, 191);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(104, 18);
+			this->label2->TabIndex = 15;
+			this->label2->Text = L"Назва книги";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(81, 141);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(95, 18);
+			this->label1->TabIndex = 14;
+			this->label1->Text = L"ПІП автора";
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->проПрограмуToolStripMenuItem,
+					this->проАвтораToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(1170, 24);
+			this->menuStrip1->TabIndex = 27;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// проПрограмуToolStripMenuItem
+			// 
+			this->проПрограмуToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->умоваToolStripMenuItem });
+			this->проПрограмуToolStripMenuItem->Name = L"проПрограмуToolStripMenuItem";
+			this->проПрограмуToolStripMenuItem->Size = System::Drawing::Size(99, 20);
+			this->проПрограмуToolStripMenuItem->Text = L"Про програму";
+			// 
+			// умоваToolStripMenuItem
+			// 
+			this->умоваToolStripMenuItem->Name = L"умоваToolStripMenuItem";
+			this->умоваToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->умоваToolStripMenuItem->Text = L"Умова";
+			this->умоваToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::умоваToolStripMenuItem_Click);
+			// 
+			// проАвтораToolStripMenuItem
+			// 
+			this->проАвтораToolStripMenuItem->Name = L"проАвтораToolStripMenuItem";
+			this->проАвтораToolStripMenuItem->Size = System::Drawing::Size(82, 20);
+			this->проАвтораToolStripMenuItem->Text = L"Про автора";
+			this->проАвтораToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::проАвтораToolStripMenuItem_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1170, 510);
+			this->Controls->Add(this->menuStrip1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+		// структура книги
+		ref struct book
+		{
+			String^ FIO;     // автор
+			String^ Nazva;   // назва книги
+			int rik;         // рік
+		};
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	int r;                  // рік для порівняння
+	book^ bk = gcnew book;  // створення структури
+	String^ s1;             // рядок для виводу
+
+	// введення даних
+	bk->FIO = textBox1->Text;
+	bk->Nazva = textBox2->Text;
+	bk->rik = Convert::ToInt32(textBox3->Text);
+
+	// формуємо рядок
+	s1 = bk->FIO + " " +" " + bk->Nazva + " Рік: " + bk->rik.ToString();
+
+	// рік для фільтрації
+	r = Convert::ToInt32(textBox4->Text);
+
+	// ✔ ВСІ КНИГИ
+	textBox5->AppendText(s1 + "\r\n");
+
+	// ✔ ВІДФІЛЬТРОВАНІ
+	if (bk->rik > r)
+	{
+		textBox6->AppendText(s1 + "\r\n");
+	}
+
+	// очистка
+	textBox1->Text = "";
+	textBox2->Text = "";
+	textBox3->Text = "";
+
+}
+private: System::Void умоваToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+	MessageBox::Show("Структура: ПІП автора, назва книги, рік видання. Вивести книги, які видані пізніше введеного року");
+}
+private: System::Void проАвтораToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+	MessageBox::Show("Автор: Студент групи 2М, Бондар Наталія");
+}
+};
+}
